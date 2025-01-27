82152c98ad0b8c8ff073a961e4b2e90824c62e30
Sn00pyW00dst0ck <gabealdous@gmail.com>
2025-01-27 12:17:46 -0500

    Serve swagger documentation from swag

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -7,16 +7,21 @@ import (
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	httpSwagger "github.com/swaggo/http-swagger"
+
+	_ "app/docs"
 )
 
 // initialize a new mux router here
-var MuxRouter = mux.NewRouter()
+var MuxRouter = mux.NewRouter().StrictSlash(true)
 
 func init() {
 	// global middleware is registered here
 	MuxRouter.Use(handlers.RecoveryHandler())
 	MuxRouter.Use(middleware.TrustProxy(middleware.PrivateRanges()))
 	MuxRouter.Use(middleware.Logger())
+
+	MuxRouter.PathPrefix("/docs/").Handler(httpSwagger.WrapHandler)
 
 	// register base routes here, eg '/' and '/health'
 	base.RegisterRoutes(MuxRouter)
