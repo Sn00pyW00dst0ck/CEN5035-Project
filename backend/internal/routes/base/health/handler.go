8726c320d40a898930f9b0e1f7045e31c5e170d4
Sn00pyW00dst0ck <gabealdous@gmail.com>
2025-01-27 16:55:42 -0500

    Setup Swagger UI Docs

--- backend/internal/routes/base/health/handler.go
+++ backend/internal/routes/base/health/handler.go
@@ -2,7 +2,9 @@ package health
 
 import "net/http"
 
+// @description Returns a simple 200 status code back, this is useful for pinging the server for checking health, etc
+// @Success 200
+// @Router /health [get]
 func Handler(w http.ResponseWriter, _ *http.Request) {
-	// send a simple 200 status code back, this is where you could ping your database, etc
 	w.WriteHeader(200)
 }
