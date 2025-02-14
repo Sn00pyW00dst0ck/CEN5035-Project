41c98cdc3be5990e8343d27097a409956c4f7abf
Sn00pyW00dst0ck <gabealdous@gmail.com>
2025-02-14 18:07:28 -0500

    Account search implemented

--- backend/internal/api/v1/sector.go
+++ backend/internal/api/v1/sector.go
@@ -7,15 +7,67 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"slices"
 
 	"berty.tech/go-orbit-db/iface"
 	"github.com/oapi-codegen/runtime/types"
 	"go.uber.org/zap"
+
+	"github.com/lithammer/fuzzysearch/fuzzy"
 )
 
 type SectorAPI struct {
 	Logger *zap.Logger
 	DB     *database.Database
+}
+
+// SearchAccounts implements ServerInterface.
+func (s *SectorAPI) SearchAccounts(w http.ResponseWriter, r *http.Request) {
+	var filter AccountFilter
+	if err := json.NewDecoder(r.Body).Decode(&filter); err != nil {
+		s.Logger.Debug(err.Error())
+		http.Error(w, "Could not parse request body.", http.StatusBadRequest)
+		return
+	}
+
+	accounts, err := s.DB.Store.Query(context.Background(), func(doc interface{}) (bool, error) {
+		data, ok := doc.(map[string]interface{})
+		if !ok {
+			return false, nil
+		}
+
+		var account Account
+		err := MapToStruct(data, &account)
+		if err != nil {
+			fmt.Println(err)
+			return false, nil
+		}
+
+		// Apply filter conditions
+		if filter.Ids != nil && !slices.Contains(*filter.Ids, account.Id) {
+			return false, nil
+		}
+		if filter.From != nil && account.CreatedAt.Before(*filter.From) {
+			return false, nil
+		}
+		if filter.Until != nil && account.CreatedAt.After(*filter.Until) {
+			return false, nil
+		}
+		if filter.Username != nil && !fuzzy.Match(*filter.Username, account.Username) {
+			return false, nil
+		}
+
+		return true, nil
+	})
+	if err != nil {
+		s.Logger.Debug(err.Error())
+		http.Error(w, "Could not perform database query.", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(accounts)
 }
 
 // PutAccount implements ServerInterface.
@@ -27,14 +79,13 @@ func (s *SectorAPI) PutAccount(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// TODO: this fails every time because the 'Account' generated has 'Id' and not 'id' so we have to convert that. UGH!
 	created_account, err := s.DB.Store.Put(context.Background(), StructToMap(account_details))
 	if err != nil {
 		s.Logger.Debug(err.Error())
 		http.Error(w, "Could not update within databse.", http.StatusInternalServerError)
 		return
 	}
-	// TODO: might need to do other things to get account return type to work with the openapi validation.
+
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(created_account)
@@ -119,4 +170,16 @@ func StructToMap(obj interface{}) map[string]interface{} {
 	json.Unmarshal(data, &result)
 
 	return result
+}
+
+// Whenever we want to convert something in the database to a struct use this.
+func MapToStruct(data map[string]interface{}, obj interface{}) error {
+	// Marshal the map to JSON
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// Unmarshal JSON into the provided struct
+	return json.Unmarshal(jsonData, obj)
 }
