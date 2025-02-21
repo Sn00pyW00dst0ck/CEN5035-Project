e497af79b4240d59fe110ce2d926e0311d8d61ee
Sn00pyW00dst0ck <gabealdous@gmail.com>
2025-02-21 12:08:37 -0500

    Reconfigure oapi-codegen and sector to support testing via HTTP client

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
+	"app/internal/api"
 	v1 "app/internal/api/v1"
-	"app/internal/middleware"
 	"context"
-	"embed"
-	"encoding/json"
 	"flag"
 	"log"
 	"net"
@@ -15,57 +13,15 @@ import (
 
 	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/gorilla/mux"
-	oapimiddleware "github.com/oapi-codegen/nethttp-middleware"
 )
-
-//go:embed swagger-ui.html
-var swaggerUI embed.FS
 
 func main() {
 	port := flag.String("port", "3000", "Port for HTTP server")
 	flag.Parse()
 
-	// Setup the swagger docs.
-	swaggerV1, err := v1.GetSwagger()
-	if err != nil {
-		panic(err)
-	}
-	// Clear out the servers array in the swagger spec, that skips validating
-	// that server names match. We don't know how this thing will be run.
-	swaggerV1.Servers = nil
-	fixSwaggerPrefix("/v1/api", swaggerV1)
-
-	// Create an instance of the handler which satisfies the generated interface
-	sectorAPI := v1.NewSector(context.WithoutCancel(openapi3.NewLoader().Context), "log.txt", "cache")
-
-	// Setup the gorilla mux server with logging.
 	r := mux.NewRouter().StrictSlash(true)
-	r.Use(middleware.RequestLogger(sectorAPI.Logger))
-
-	// Serve the swagger.json file directly at /docs/swagger.json
-	r.HandleFunc("/v1/swagger.json", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(swaggerV1)
-	})
-
-	// Serve the Swagger UI using a CDN
-	r.HandleFunc("/v1/swagger-ui/", func(w http.ResponseWriter, r *http.Request) {
-		data, err := swaggerUI.ReadFile("swagger-ui.html")
-		if err != nil {
-			http.Error(w, "swagger.html not found", http.StatusNotFound)
-			return
-		}
-		w.Header().Set("Content-Type", "text/html")
-		w.Write([]byte(data))
-	})
-
-	// Subrouter to validate requests to the /v1/api/
-	v1.HandlerWithOptions(sectorAPI, v1.GorillaServerOptions{
-		BaseURL:     "/v1/api",
-		BaseRouter:  r,
-		Middlewares: []v1.MiddlewareFunc{oapimiddleware.OapiRequestValidator(swaggerV1)},
-	})
+	sectorAPI := v1.NewSector(context.WithoutCancel(openapi3.NewLoader().Context), "log.txt", "cache")
+	api.AddV1SectorAPIToRouter(r, sectorAPI)
 
 	c := cors.New(cors.Options{
 		AllowedOrigins:   []string{"http://localhost:8000"},
@@ -78,15 +34,4 @@ func main() {
 		Addr:    net.JoinHostPort("127.0.0.1", *port),
 	}
 	log.Fatal(s.ListenAndServe())
-}
-
-// Necessary so that we can use the OapiRequestValidator with a BaseURL
-func fixSwaggerPrefix(prefix string, swagger *openapi3.T) {
-	var updatedPaths openapi3.Paths = openapi3.Paths{}
-
-	for key, value := range swagger.Paths.Map() {
-		updatedPaths.Set(prefix+key, value)
-	}
-
-	swagger.Paths = &updatedPaths
 }
--- backend/tests/api/v1/sector_test.go
+++ backend/tests/api/v1/sector_test.go
@@ -1,18 +1,46 @@
 package v1Test
 
 import (
+	"app/internal/api"
 	v1 "app/internal/api/v1"
 	"app/internal/database"
 	"context"
+	"log"
+	"net"
+	"net/http"
 	"testing"
+
+	"github.com/gorilla/mux"
 )
 
-func TestSector(t *testing.T) {
+func TestSectorV1(t *testing.T) {
 	// Setup temporary database cache location
 	tmpDir, clean := database.TestingTempDir(t, "sectordb_cache_test")
 	defer clean()
 
-	v1.NewTestingSector(context.Background(), "log_test.txt", tmpDir, t)
+	r := mux.NewRouter().StrictSlash(true)
+	testSectorAPI := v1.NewTestingSector(context.Background(), "log_test.txt", tmpDir, t)
+	api.AddV1SectorAPIToRouter(r, testSectorAPI)
 
-	// TODO: figure out a way to auto-generate test stubs for each endpoint!!
+	s := &http.Server{
+		Handler: r,
+		Addr:    net.JoinHostPort("127.0.0.1", "3000"),
+	}
+	log.Fatal(s.ListenAndServe())
+	defer s.Close()
+
+	// Setup the test client
+	// testClient, err := v1.NewClientWithResponses("localhost:3000")
+
+	// TODO: use auto-generated test-client to query each endpoint and test responses
+	cases := []struct{ Name, Other string }{
+		{"Get Root", ""},
+		{"Get Health", ""},
+	}
+
+	for _, c := range cases {
+		t.Run(c.Name, func(t *testing.T) {
+
+		})
+	}
 }
