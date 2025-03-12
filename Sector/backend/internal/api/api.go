d23d128e1e58f5b0be0e5f52cc330a62fdd45483
Sn00pyW00dst0ck <gabealdous@gmail.com>
2025-03-12 16:15:59 -0400

    Wails will build and serve frontend, backend not linked up yet

---
+++ Sector/backend/internal/api/api.go
@@ -0,0 +1,67 @@
+package api
+
+import (
+	v1 "app/internal/api/v1"
+	"app/internal/middleware"
+	"embed"
+	"encoding/json"
+	"net/http"
+
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/gorilla/mux"
+	oapimiddleware "github.com/oapi-codegen/nethttp-middleware"
+)
+
+//go:embed v1/swagger-ui.html
+var swaggerUI embed.FS
+
+// A helper function to take any SectorAPI instance and add it to the given mux router instance.
+func AddV1SectorAPIToRouter(router *mux.Router, api *v1.SectorAPI) {
+	// Setup the swagger docs.
+	swaggerV1, err := v1.GetSwagger()
+	if err != nil {
+		panic(err)
+	}
+	// Clear out the servers array in the swagger spec, that skips validating
+	// that server names match. We don't know how this thing will be run.
+	swaggerV1.Servers = nil
+	fixSwaggerPrefix("/v1/api", swaggerV1)
+
+	router.Use(middleware.RequestLogger(api.Logger))
+
+	// Serve the swagger.json file directly at /docs/swagger.json
+	router.HandleFunc("/v1/swagger.json", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(swaggerV1)
+	})
+
+	// Serve the Swagger UI using a CDN
+	router.HandleFunc("/v1/swagger-ui/", func(w http.ResponseWriter, r *http.Request) {
+		data, err := swaggerUI.ReadFile("v1/swagger-ui.html")
+		if err != nil {
+			http.Error(w, "swagger-ui.html not found", http.StatusNotFound)
+			return
+		}
+		w.Header().Set("Content-Type", "text/html")
+		w.Write([]byte(data))
+	})
+
+	// Subrouter to validate requests to the /v1/api/
+	v1.HandlerWithOptions(api, v1.GorillaServerOptions{
+		BaseURL:     "/v1/api",
+		BaseRouter:  router,
+		Middlewares: []v1.MiddlewareFunc{oapimiddleware.OapiRequestValidator(swaggerV1)},
+	})
+}
+
+// Necessary so that we can use the OapiRequestValidator with a BaseURL
+func fixSwaggerPrefix(prefix string, swagger *openapi3.T) {
+	var updatedPaths openapi3.Paths = openapi3.Paths{}
+
+	for key, value := range swagger.Paths.Map() {
+		updatedPaths.Set(prefix+key, value)
+	}
+
+	swagger.Paths = &updatedPaths
+}
