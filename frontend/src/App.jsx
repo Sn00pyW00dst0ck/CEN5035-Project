b37eb1271e25c1728a02d8cbd41ae1ff52a13e15
abhignan <arcotas115@gmail.com>
2025-03-24 17:37:41 -0400

    Created a Login Page(No authentication).

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,28 @@
 import './App.css'
-
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import MainScreen from "./MainScreen/MainScreen.jsx";
+import Login from "./Login/Login.jsx";
+import { useState } from 'react';
 
-//Temporary should be fixed.
 export const YourUser = {id: 1, name: "YourUsername", status: "Hi", online: true, icon: "public/vite.svg"}
 
 const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
-
 const theme = createTheme({
-    palette: {
-        mode: darkThemeMq.matches ? 'dark' : 'light',
-    },
+  palette: {
+    mode: darkThemeMq.matches ? 'dark' : 'light',
+  },
 });
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    return (
-        <div>
-            <ThemeProvider theme={theme}>
-                <MainScreen/>
-            </ThemeProvider>
-        </div>
-    )
+  return (
+    <div>
+      <ThemeProvider theme={theme}>
+        {isLoggedIn ? <MainScreen /> : <Login onLogin={setIsLoggedIn} />}
+      </ThemeProvider>
+    </div>
+  )
 }
 
 export default App
-
